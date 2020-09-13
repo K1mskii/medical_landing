ebaf8a3ebfd405c46635819abde557db3a5b4303
K1mskii <movies2702@gmail.com>
2020-09-13 12:26:23 +0600

    upd

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Scroll page
+$("a.scroll-to").on("click", function (e) {
+    e.preventDefault();
+    var anchor = $(this).attr("href");
+    $("html, body")
+        .stop()
+        .animate({
+                scrollTop: $(anchor).offset().top - 0,
+            },
+            800
+        );
+});
